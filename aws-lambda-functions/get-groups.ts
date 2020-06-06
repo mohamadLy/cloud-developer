e6b97855b303affd222c8fa2501a5a3daaa58251
Mohamadou Ly <mohamadou.ly@hpe.com>
2020-06-06 09:26:05 -0400

    feat: Created environment variable for DynamoDB table, read and return data from table

--- aws-lambda-functions/get-groups.ts
+++ aws-lambda-functions/get-groups.ts
@@ -3,13 +3,17 @@ import AWS from 'aws-sdk'
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 async (event) => {
+    const result = await docClient.scan({
+        TableName: process.env.GROUP_TABLE
+    }).promise()
+    const items = result.Items
     return {
         statusCode: 200,
         header: {
             'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify({
-            items: mockData
+            items: items
         })
     }
 };
