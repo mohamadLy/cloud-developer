72b079ce3cd7c978bcf6b71f921560df42b2a862
Mohamadou Ly <mohamadou.ly@hpe.com>
2020-06-06 10:17:11 -0400

    feat: Import v4 from uuidv4 lib and generated itemId from it for every new post request

--- aws-lambda-functions/create-groups/index.ts
+++ aws-lambda-functions/create-groups/index.ts
@@ -1,3 +1,10 @@
 import AWS from 'aws-sdk'
+import v4 from 'uuidv4'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+
+async ( event ) => {
+    console.log("Processing event: ", event)
+    const itemId = v4.uuid()
+
+}
