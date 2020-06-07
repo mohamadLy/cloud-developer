9c577e72f4c42ab00fde80f033f485418fdd681b
Mohamadou Ly <mohamadou.ly@hpe.com>
2020-06-07 14:53:02 -0400

    Use typescript syntax

--- aws-lambda-functions/create-groups/index.ts
+++ aws-lambda-functions/create-groups/index.ts
@@ -1,10 +1,32 @@
-import AWS from 'aws-sdk'
-import v4 from 'uuidv4'
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+const groupTable = process.env.GROUP_TABLE
 
-async ( event ) => {
+export const handler = async ( event ) => {
     console.log("Processing event: ", event)
-    const itemId = v4.uuid()
+    const itemId = uuid.v4()
 
+    const parsedBody = JSON.parse(event.body)
+
+    const newItem = {
+        id: itemId,
+        ...parsedBody
+    }
+
+   docClient.put({
+       TableName: groupTable,
+       Item: newItem
+   }).promise()
+
+    return ({
+        statusCode: 201,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            item: newItem
+        })
+    })
 }
